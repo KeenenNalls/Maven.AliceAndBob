8a883844e420c2a192b77b7897387b2fbf9e35aa
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-06-30 15:07:08 -0400

    Finished the lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+
+        System.out.println("Hello users! Please enter your names: ");
+        Scanner in = new Scanner(System.in);
+        String username = in.nextLine();
+        String alice = "alice";
+        String bob = "bob";
+
+        if (alice.equals(username)) {
+            System.out.println("Hello alice, welcome back!");
+        }
+        if (alice.equals(username)) {
+            System.out.println("Hello bob, welcome back!");
+        }
+
+
 
     }
 }
